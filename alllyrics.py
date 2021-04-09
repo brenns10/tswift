2370ebadf330f8ad9bc24a6eaf01b6999b5edbce
Stephen Brennan <stephen@brennan.io>
2021-04-08 21:43:03 -0700

    json output in alllyrics.py
    In case one would want to improve the output of their Taylor Swift bot,
    for example...

--- alllyrics.py
+++ alllyrics.py
@@ -2,14 +2,14 @@
 """
 Download all the song lyrics for a particular artist, store in a file.
 """
-
-from __future__ import print_function
+import argparse
+import json
 from multiprocessing.pool import ThreadPool
 
 from tswift import Artist, Song
 
 
-def download_all_songs(artist, outfn):
+def download_all_songs(artist, outfn, as_json=False):
     artist_object = Artist(artist)
     artist_object.load(verbose=True)
     outfile = open(outfn, 'w')
@@ -18,14 +18,28 @@ def download_all_songs(artist, outfn):
     songs = artist_object.songs
     print('loading %d songs in parallel!' % len(artist_object.songs))
     pool.map(Song.load, songs)
-    for song in songs:
-        print(song.lyrics, file=outfile)
-        print("\n\n", file=outfile)
+    if json:
+        objs = []
+        for song in songs:
+            objs.append({
+                "title": song.title,
+                "artist": song.artist,
+                "url": song._url,
+                "lyrics": song.lyrics,
+            })
+        json.dump(objs, outfile)
+    else:
+        for song in songs:
+            print(song.lyrics, file=outfile)
+            print("\n\n", file=outfile)
+
+    outfile.close()
 
 
 if __name__ == '__main__':
-    import sys
-    if len(sys.argv) != 3:
-        print('Need to pass artist and output file.')
-        sys.exit(1)
-    download_all_songs(*sys.argv[1:])
+    parser = argparse.ArgumentParser(description="lyric downloader")
+    parser.add_argument("artist", help="Artist to download")
+    parser.add_argument("outfn", help="Output filename")
+    parser.add_argument("--json", action="store_true", help="Use JSON output")
+    args = parser.parse_args()
+    download_all_songs(args.artist, args.outfn, args.json)
